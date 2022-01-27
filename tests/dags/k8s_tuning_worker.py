3a62ed4bdbd8c118cffbbf47279d6034f61ff641
yeungjunkim <pooh97@gmail.com>
2022-01-27 12:05:46 +0900

    Update k8s_tuning_worker.py

--- tests/dags/k8s_tuning_worker.py
+++ tests/dags/k8s_tuning_worker.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
