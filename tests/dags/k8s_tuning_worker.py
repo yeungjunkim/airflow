f60a3036dc79b20c3b37ccb1ba14a9570094b0de
yeungjunkim <pooh97@gmail.com>
2022-01-27 11:49:15 +0900

    Create k8s_tuning_worker.py

---
+++ tests/dags/k8s_tuning_worker.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'accutuning-worker-test', default_args=default_args, schedule_interval=timedelta(minutes=10))
+
+
+start = DummyOperator(task_id='start', dag=dag)
+
+worker = KubernetesPodOperator(namespace='default',
+                          image="accutuning:latest",
+#                           cmds=["python","-c"],
+#                           arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="accutuning-test",
+                          task_id="accutuning",
+                          get_logs=True,
+                          dag=dag
+                          )
+
+end = DummyOperator(task_id='end', dag=dag)
+
+
+worker.set_upstream(start)
+worker.set_downstream(end)
