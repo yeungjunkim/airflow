3e6f3d99b91d6e80ff1cb376ccd0784beab12a7d
yeungjunkim <pooh97@gmail.com>
2022-02-03 11:47:22 +0900

    Update example_kubernetes.py

--- tests/dags/example_kubernetes.py
+++ tests/dags/example_kubernetes.py
@@ -1,3 +1,14 @@
+
+from datetime import datetime
+
+from kubernetes.client import models as k8s
+
+from airflow import DAG
+from airflow.kubernetes.secret import Secret
+from airflow.operators.bash import BashOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+# [START howto_operator_k8s_cluster_resources]
 secret_file = Secret('volume', '/etc/sql_conn', 'airflow-secrets', 'sql_alchemy_conn')
 secret_env = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
 secret_all_keys = Secret('env', None, 'airflow-secrets-2')
@@ -63,3 +74,70 @@ affinity = k8s.V1Affinity(
 )
 
 tolerations = [k8s.V1Toleration(key="key", operator="Equal", value="value")]
+
+# [END howto_operator_k8s_cluster_resources]
+
+
+with DAG(
+    dag_id='example_kubernetes_operator',
+    schedule_interval=None,
+    start_date=datetime(2021, 1, 1),
+    tags=['example'],
+) as dag:
+    k = KubernetesPodOperator(
+        namespace='default',
+        image="ubuntu:16.04",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        secrets=[secret_file, secret_env, secret_all_keys],
+        ports=[port],
+        volumes=[volume],
+        volume_mounts=[volume_mount],
+        env_from=configmaps,
+        name="airflow-test-pod",
+        task_id="task",
+        affinity=affinity,
+        is_delete_operator_pod=True,
+        hostnetwork=False,
+        tolerations=tolerations,
+        init_containers=[init_container],
+        priority_class_name="medium",
+    )
+
+    # [START howto_operator_k8s_private_image]
+    quay_k8s = KubernetesPodOperator(
+        namespace='default',
+        image='quay.io/apache/bash',
+        image_pull_secrets=[k8s.V1LocalObjectReference('testquay')],
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10", "echo pwd"],
+        labels={"foo": "bar"},
+        name="airflow-private-image-pod",
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="task-two",
+        get_logs=True,
+    )
+    # [END howto_operator_k8s_private_image]
+
+    # [START howto_operator_k8s_write_xcom]
+    write_xcom = KubernetesPodOperator(
+        namespace='default',
+        image='alpine',
+        cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
+        name="write-xcom",
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="write-xcom",
+        get_logs=True,
+    )
+
+    pod_task_xcom_result = BashOperator(
+        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
+        task_id="pod_task_xcom_result",
+    )
+    # [END howto_operator_k8s_write_xcom]
+
+    write_xcom >> pod_task_xcom_result
