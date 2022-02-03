c28231f89fd84f0a98df4d7befda9392497e5dae
yeungjunkim <pooh97@gmail.com>
2022-02-03 13:47:02 +0900

    Update k8s_tuning_worker.py

--- tests/dags/k8s_tuning_worker.py
+++ tests/dags/k8s_tuning_worker.py
@@ -92,7 +92,7 @@ worker = KubernetesPodOperator(
 #     cmds=["sleep", "0.03h"],
 #     cmds=["bash", "/code/entrypoint.sh"],
     cmds=["bash", "-cx"],
-    arguments=["export", "ACCUTUNING_LOG_LEVEL='INFO'"],
+#     arguments=["export", "ACCUTUNING_LOG_LEVEL='INFO'"],
     arguments=["export", "ACCUTUNING_WORKSPACE='/workspace/experiment_0008/experimentprocess_0037'"],
 #     arguments=["bash", "/code/entrypoint.sh"],
   
