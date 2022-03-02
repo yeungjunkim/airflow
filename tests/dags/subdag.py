491b3d685080e9ea8ca4acb9da34d63237ba456c
yeungjunkim <pooh97@gmail.com>
2022-03-02 17:41:50 +0900

    Update subdag.py

--- tests/dags/subdag.py
+++ tests/dags/subdag.py
@@ -1,18 +1,34 @@
+# subdag.py
+
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+def random_subdag(parent_dag_name, child_dag_name, args):
+    dag_subdag = DAG(
+        dag_id='%s.%s' % (parent_dag_name, child_dag_name), # subdag의 id는 이와같은 컨벤션으로 쓴답니다.
+        default_args=args,
+        schedule_interval=None, # 값을 넣어주어야 합니다.
+    )
+    union =DummyOperator(
+        task_id='%s-union' % (child_dag_name),
+        default_args=args,
+        dag=dag_subdag
+        )
 
+    for i in range(2):
+        globals()['process_a'] = BashOperator(
+            task_id='%s-task_A-%s' % (child_dag_name, i + 1),
+            default_args=args,
+            bash_command='echo "does it work?"',
+            dag=dag_subdag,
+        )
+        globals()['process_b'] = BashOperator(
+            task_id='%s-task_B-%s' % (child_dag_name, i + 1),
+            default_args=args,
+            bash_command='date',
+            dag=dag_subdag,
+        )
 
-# Dag is returned by a factory method
-def sub_dag(parent_dag_name, child_dag_name, start_date, schedule_interval):
-  dag = DAG(
-    '%s.%s' % (parent_dag_name, child_dag_name),
-    schedule_interval=schedule_interval,
-    start_date=start_date,
-  )
+        process_a >> process_b >> union
 
-  dummy_operator = DummyOperator(
-    task_id='dummy_task',
-    dag=dag,
-  )
-
-  return dag
+    return dag_subdag
