f3e013b99f7350a6201fb5540b5a0ef0931fb15e
yeungjunkim <pooh97@gmail.com>
2022-03-03 09:11:29 +0900

    Create my_maindag.py

---
+++ tests/dags/my_maindag.py
@@ -0,0 +1,22 @@
+# main_dag.py
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.operators.subdag_operator import SubDagOperator
+from my_subdag import sub_dag
+
+
+PARENT_DAG_NAME = 'parent_dag'
+CHILD_DAG_NAME = 'child_dag'
+
+main_dag = DAG(
+  dag_id=PARENT_DAG_NAME,
+  schedule_interval=timedelta(hours=1),
+  start_date=datetime(2016, 1, 1)
+)
+
+sub_dag = SubDagOperator(
+  subdag=sub_dag(PARENT_DAG_NAME, CHILD_DAG_NAME, main_dag.start_date,
+                 main_dag.schedule_interval),
+  task_id=CHILD_DAG_NAME,
+  dag=main_dag,
+)
