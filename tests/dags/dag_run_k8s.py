52b88658d32d5f27e6b9adebe4e6fc3cc2ec0912
yeungjunkim <pooh97@gmail.com>
2022-03-21 16:11:54 +0900

    Update dag_run_k8s.py

--- tests/dags/dag_run_k8s.py
+++ tests/dags/dag_run_k8s.py
@@ -18,7 +18,8 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=5),
+    'render_template_as_native_obj': True,
 }
 
 dag = DAG(
