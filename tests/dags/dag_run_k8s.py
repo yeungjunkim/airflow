e9f79a9a30e9d3e5b6e28dcfb2dc93c1ebab890d
yeungjunkim <pooh97@gmail.com>
2022-03-24 15:47:33 +0900

    Update dag_run_k8s.py

--- tests/dags/dag_run_k8s.py
+++ tests/dags/dag_run_k8s.py
@@ -95,8 +95,12 @@ def make_parameters(**kwargs):
     print("experiment_process_type = {}".format(experiment_process_type))
     container_uuid = make_uuid()
     django_command = get_command_name(experiment_process_type)
-    kwargs['dag_run'].conf['ACCUTUNING_UUID'] = container_uuid
-    kwargs['dag_run'].conf['ACCUTUNING_DJANGO_COMMAND'] = django_command
+    # kwargs['dag_run'].conf['ACCUTUNING_UUID'] = container_uuid
+    # kwargs['dag_run'].conf['ACCUTUNING_DJANGO_COMMAND'] = django_command
+
+    kwargs['task_instance'].xcom_push(key='ACCUTUNING_UUID', value=container_uuid)
+    kwargs['task_instance'].xcom_push(key='ACCUTUNING_DJANGO_COMMAND', value=django_command)
+
 
     # print("kwargs['dag_run'].conf['ACCUTUNING_UUID'] = {}".format(kwargs['dag_run'].conf['ACCUTUNING_UUID']))
     # print("kwargs['dag_run'].conf['ACCUTUNING_DJANGO_COMMAND'] = {}".format(kwargs['dag_run'].conf['ACCUTUNING_DJANGO_COMMAND']))
@@ -121,6 +125,8 @@ def make_worker_env(**kwargs):
 
     env_dict = json.loads(worker_env_vars_str)
     env_dict['ACCUTUNING_WORKSPACE'] = workspace_path
+    kwargs['task_instance'].xcom_push(key='ACCUTUNING_WORKER_WORKSPACE', value=workspace_path)
+
     worker_env_vars = json.dumps(env_dict)
 
     print(f'worker_env_vars:{worker_env_vars}')
@@ -161,9 +167,9 @@ before_worker = KubernetesPodOperator(
     },
     cmds=["python3"],
     arguments=["/code/manage.py", 
-               "{{dag_run.conf['ACCUTUNING_DJANGO_COMMAND']}}", 
+               "{{ ti.xcom_pull(key='ACCUTUNING_DJANGO_COMMAND) }}",
                "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  
-               "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", 
+               "--uuid={{ ti.xcom_pull(key='ACCUTUNING_WORKER_WORKSPACE) }}'", 
                "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}",
                "--execute_range=before",
                "--experiment_process_type={{dag_run.conf['experiment_process_type']}", 
@@ -185,7 +191,7 @@ worker = KubernetesPodOperator(
     volume_mounts=[volume_mount],
     name="worker",
     task_id="worker",
-    env_vars={'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf["ACCUTUNING_LOG_LEVEL"]}}', 'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKER_WORKSPACE"]}}'},
+    env_vars={'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf["ACCUTUNING_LOG_LEVEL"]}}', 'ACCUTUNING_WORKSPACE':'{{ ti.xcom_pull(key="ACCUTUNING_WORKER_WORKSPACE") }}'},
     get_logs=True,
     dag=dag,    
 )
@@ -208,13 +214,17 @@ worker_success = KubernetesPodOperator(
     # cmds=["python3"],
     # arguments=["/code/manage.py", ""{{dag_run.conf['ACCUTUNING_DJANGO_COMMAND']}}"", "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}"],   
     cmds=["python3"],
-    arguments=["/code/manage.py", "{{dag_run.conf['ACCUTUNING_DJANGO_COMMAND']}}", "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}","--execute_range=after"],   
-#     arguments="{{dag_run.conf.after_command}}",       
-#     arguments=["/code/manage.py", "ml_parse", "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}","--execute_range=after"],   
-    
-#     arguments=["/code/manage.py", "ml_parse", "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}","--execute_range=after"],   
-#     cmds=['{{dag_run.conf.after_command1}}'],
-#     arguments=['{{dag_run.conf.after_command2}}'],       
+    arguments=["/code/manage.py", 
+               "{{ ti.xcom_pull(key='ACCUTUNING_DJANGO_COMMAND) }}",
+               "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  
+               "--uuid={{ ti.xcom_pull(key='ACCUTUNING_WORKER_WORKSPACE) }}'", 
+               "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}",
+               "--execute_range=after"
+               "--experiment_process_type={{dag_run.conf['experiment_process_type']}", 
+               "--experiment_targe={{dag_run.conf['experiment_target']}", 
+               "--proceed_next={{dag_run.conf['proceed_next']}",  
+               ],   
+   
     get_logs=True,
     dag=dag,        
     trigger_rule='all_success',
@@ -240,9 +250,9 @@ worker_fail = KubernetesPodOperator(
 #     arguments=["/code/manage.py", "ml_parse", "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}","--execute_range=after"],   
     cmds=["python3"],
     arguments=["/code/manage.py",
-               "{{dag_run.conf['ACCUTUNING_DJANGO_COMMAND']}}", 
+               "{{ ti.xcom_pull(key='ACCUTUNING_DJANGO_COMMAND) }}",
                "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  
-               "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", 
+               "--uuid={{ ti.xcom_pull(key='ACCUTUNING_WORKER_WORKSPACE) }}'", 
                "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}",
                "--execute_range=after"
                "--experiment_process_type={{dag_run.conf['experiment_process_type']}", 
