6622be03bcf42c3a4b5f41d025134e945ebe0f90
yeungjunkim <pooh97@gmail.com>
2022-03-29 13:46:14 +0900

    분기문 추가

--- tests/dags/dag_run_k8s.py
+++ tests/dags/dag_run_k8s.py
@@ -72,7 +72,9 @@ def get_next_experiment_process_type(experiment_process_type, use_ensemble):
     print("experiment_process_type == 'optuna' = {}".format(experiment_process_type == 'optuna'))
     print("use_ensemble == 'False' = {}".format(use_ensemble == 'False'))
 
-    if experiment_process_type=="preprocess" or experiment_process_type=="optuna" or experiment_process_type=="ml_ensemble":
+    if experiment_process_type=="preprocess" or \
+       experiment_process_type=="optuna" or \
+       experiment_process_type=="ensemble":
         if experiment_process_type == 'optuna' and use_ensemble == 'False':  # to deploy
             return command_list[command_list.index(experiment_process_type) + 2] 
         else:
