185d2cbfcda4aed9426b270d397602cf41ea9df8
yeungjunkim <pooh97@gmail.com>
2022-04-12 17:40:58 +0900

    Update dag_run_k8s.py

--- tests/dags/dag_run_k8s.py
+++ tests/dags/dag_run_k8s.py
@@ -5,7 +5,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 # from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-# from kubernetes.client import models as k8s  # you should write this sentence when you could use volume, etc 
+from kubernetes.client import models as k8s  # you should write this sentence when you could use volume, etc 
 # from airflow.operators.python_operator import BranchPythonOperator
 # from airflow.utils.edgemodifier import Label  #label 쓰기 위한 library
 # import json
