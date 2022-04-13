631c5b9cae91f87c25aea46702c617df9bfbcd01
yeungjunkim <pooh97@gmail.com>
2022-04-13 19:47:59 +0900

    Update ml_automl.py

--- tests/dags/ml_automl.py
+++ tests/dags/ml_automl.py
@@ -31,7 +31,7 @@ class TriggerDagRunWithConfigOperator(TriggerDagRunOperator):
             trigger_dag_id = 'ml_run_k8s'
         else:
             trigger_dag_id = 'ml_run_docker'
-        self.trigger_dag_id = trigger_dag_id
+        kwargs['trigger_dag_id'] = trigger_dag_id
         super().__init__(*args, **kwargs)
 
     def pre_execute(self, *args, **kwargs):
