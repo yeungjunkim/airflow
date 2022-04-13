ae7ec8f49893b75836fbddd05dbd8ea4875ad8a5
yeungjunkim <pooh97@gmail.com>
2022-04-13 19:16:57 +0900

    Update ml_automl.py

--- tests/dags/ml_automl.py
+++ tests/dags/ml_automl.py
@@ -25,7 +25,11 @@ class TriggerDagRunWithConfigOperator(TriggerDagRunOperator):
         kwargs['wait_for_completion'] = True
         kwargs['poke_interval'] = 1
         kwargs['reset_dag_run'] = True
-        kwargs['trigger_dag_id'] = 'ml_run_k8s'  # ml_run_k8s 와 어떻게 분기할까요?
+        if kwargs['context']['dag_run'].conf.get('ACCUTUNING_K8S_USE'):
+            trigger_dag_id = 'ml_run_k8s'
+        else:
+            trigger_dag_id = 'ml_run_docker'
+        kwargs['trigger_dag_id'] = trigger_dag_id  # ml_run_k8s 와 어떻게 분기할까요?
         kwargs['conf'] = kwargs.get('conf') or dict(experiment_process_type=kwargs['task_id'])
         super().__init__(*args, **kwargs)
 
