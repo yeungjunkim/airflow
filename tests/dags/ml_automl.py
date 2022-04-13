e5dcf48dde77d7e1527035f8b64c6fd5d88a2363
yeungjunkim <pooh97@gmail.com>
2022-04-13 21:01:36 +0900

    Update ml_automl.py

--- tests/dags/ml_automl.py
+++ tests/dags/ml_automl.py
@@ -40,8 +40,8 @@ class TriggerDagRunWithConfigOperator(TriggerDagRunOperator):
         self.conf = conf
         pprint(self.conf)
         print("self.conf = {}".format(self.conf))
-        print("self.conf.get('ACCUTUNING_K8S_USE') = {}".format(self.conf.get('ACCUTUNING_K8S_USE')))
-        if self.conf.get('ACCUTUNING_K8S_USE'):
+        print("self.conf['ACCUTUNING_K8S_USE'] = {}".format(self.conf['ACCUTUNING_K8S_USE']))
+        if self.conf['ACCUTUNING_K8S_USE']:
             trigger_dag_id = 'ml_run_k8s'
         else:
             trigger_dag_id = 'ml_run_docker'
