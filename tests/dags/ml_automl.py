aac04e6126ed02c503f0cbc5946721296d1604e7
yeungjunkim <pooh97@gmail.com>
2022-04-18 15:20:12 +0900

    Update ml_automl.py

--- tests/dags/ml_automl.py
+++ tests/dags/ml_automl.py
@@ -42,10 +42,13 @@ class TriggerDagRunWithConfigOperator(TriggerDagRunOperator):
         pprint(self.conf)
 
         if json.loads(self.conf['accutuning_env_vars'])['ACCUTUNING_K8S_USE']:
-            trigger_dag_id = 'ml_run_k8s'
+            if self.conf['experiment_process_type'] == 'optuna_monitor' or \
+                    self.conf['experiment_process_type'] == 'ensemble_monitor':
+                trigger_dag_id = 'ml_run_k8s'
+            else:
+                trigger_dag_id = 'accutuning_command_on_k8s'
         else:
             trigger_dag_id = 'ml_run_docker'
-
         self.trigger_dag_id = trigger_dag_id
 
         return super().pre_execute(*args, **kwargs)
