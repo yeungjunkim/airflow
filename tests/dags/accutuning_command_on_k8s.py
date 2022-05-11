c5abe59e7cd72aa99700d762921659ff40ac418b
yeungjunkim <pooh97@gmail.com>
2022-05-11 17:24:15 +0900

    Update accutuning_command_on_k8s.py

--- tests/dags/accutuning_command_on_k8s.py
+++ tests/dags/accutuning_command_on_k8s.py
@@ -5,7 +5,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from kubernetes.client import models as k8s  # you should write this sentence when you could use volume, etc 
-
+import json
 
 default_args = {
     'owner': 'airflow',
@@ -27,18 +27,33 @@ start = DummyOperator(task_id='start', dag=dag)
 
 
 def make_env_var():
+
+    # env_vars_dict = json.loads(self.conf['accutuning_env_vars'])['ACCUTUNING_K8S_USE']
+    env_vars_dict = json.loads('{{dag_run.conf.accutuning_env_vars}}')
+
     env_dict = {
-        'ACCUTUNING_WORKSPACE': '{{dag_run.conf.ACCUTUNING_WORKSPACE}}',
-        'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf.ACCUTUNING_LOG_LEVEL}}',
-        'ACCUTUNING_USE_LABELER': '{{dag_run.conf.ACCUTUNING_USE_LABELER}}',
-        'ACCUTUNING_USE_CLUSTERING': '{{dag_run.conf.ACCUTUNING_USE_CLUSTERING}}',
-        'DJANGO_SETTINGS_MODULE': '{{dag_run.conf.DJANGO_SETTINGS_MODULE}}',
-        'ACCUTUNING_DB_ENGINE': '{{dag_run.conf.ACCUTUNING_DB_ENGINE}}',
-        'ACCUTUNING_DB_HOST': '{{dag_run.conf.ACCUTUNING_DB_HOST}}',
-        'ACCUTUNING_DB_PORT': '{{dag_run.conf.ACCUTUNING_DB_PORT}}',
-        'ACCUTUNING_DB_NAME': '{{dag_run.conf.ACCUTUNING_DB_NAME}}',
-        'ACCUTUNING_DB_USER': '{{dag_run.conf.ACCUTUNING_DB_USER}}',
-        'ACCUTUNING_DB_PASSWORD': '{{dag_run.conf.ACCUTUNING_DB_PASSWORD}}',
+        # 'ACCUTUNING_WORKSPACE': '{{dag_run.conf.ACCUTUNING_WORKSPACE}}',
+        # 'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf.ACCUTUNING_LOG_LEVEL}}',
+        # 'ACCUTUNING_USE_LABELER': '{{dag_run.conf.ACCUTUNING_USE_LABELER}}',
+        # 'ACCUTUNING_USE_CLUSTERING': '{{dag_run.conf.ACCUTUNING_USE_CLUSTERING}}',
+        # 'DJANGO_SETTINGS_MODULE': '{{dag_run.conf.DJANGO_SETTINGS_MODULE}}',
+        # 'ACCUTUNING_DB_ENGINE': '{{dag_run.conf.ACCUTUNING_DB_ENGINE}}',
+        # 'ACCUTUNING_DB_HOST': '{{dag_run.conf.ACCUTUNING_DB_HOST}}',
+        # 'ACCUTUNING_DB_PORT': '{{dag_run.conf.ACCUTUNING_DB_PORT}}',
+        # 'ACCUTUNING_DB_NAME': '{{dag_run.conf.ACCUTUNING_DB_NAME}}',
+        # 'ACCUTUNING_DB_USER': '{{dag_run.conf.ACCUTUNING_DB_USER}}',
+        # 'ACCUTUNING_DB_PASSWORD': '{{dag_run.conf.ACCUTUNING_DB_PASSWORD}}',
+        'ACCUTUNING_WORKSPACE': env_vars_dict.get('ACCUTUNING_WORKSPACE'),
+        'ACCUTUNING_LOG_LEVEL': env_vars_dict.get('ACCUTUNING_LOG_LEVEL'),
+        'ACCUTUNING_USE_LABELER': env_vars_dict.get('ACCUTUNING_USE_LABELER'),
+        'ACCUTUNING_USE_CLUSTERING': env_vars_dict.get('ACCUTUNING_USE_CLUSTERING'),
+        'DJANGO_SETTINGS_MODULE': env_vars_dict.get('DJANGO_SETTINGS_MODULE'),
+        'ACCUTUNING_DB_ENGINE': env_vars_dict.get('ACCUTUNING_DB_ENGINE'),
+        'ACCUTUNING_DB_HOST': env_vars_dict.get('ACCUTUNING_DB_HOST'),
+        'ACCUTUNING_DB_PORT': env_vars_dict.get('ACCUTUNING_DB_PORT'),
+        'ACCUTUNING_DB_NAME': env_vars_dict.get('ACCUTUNING_DB_NAME'),
+        'ACCUTUNING_DB_USER': env_vars_dict.get('ACCUTUNING_DB_USER'),
+        'ACCUTUNING_DB_PASSWORD': env_vars_dict.get('ACCUTUNING_DB_PASSWORD'),
     }
     return env_dict
 
@@ -63,17 +78,23 @@ class KubernetesPodExOperator(KubernetesPodOperator):
         super().__init__(*args, **kwargs)
 
     def pre_execute(self, *args, **kwargs):
+        env_vars_dict = json.loads('{{dag_run.conf.accutuning_env_vars}}')
+
         volume_mounts = k8s.V1VolumeMount(
-            name=kwargs['context']['dag_run'].conf.get('ACCUTUNING_PVC_NAME'),
-            mount_path=kwargs['context']['dag_run'].conf.get('ACCUTUNING_WORKSPACE'),
+            # name=kwargs['context']['dag_run'].conf.get('ACCUTUNING_PVC_NAME'),
+            # mount_path=kwargs['context']['dag_run'].conf.get('ACCUTUNING_WORKSPACE'),
+            # sub_path=None, read_only=False
+            name=env_vars_dict.get('ACCUTUNING_PVC_NAME'),
+            mount_path=env_vars_dict.get('ACCUTUNING_WORKSPACE'),
             sub_path=None, read_only=False
         )
         self.volume_mounts = [volume_mounts]
 
         volumes = k8s.V1Volume(
-            name=kwargs['context']['dag_run'].conf.get('ACCUTUNING_PVC_NAME'),
-            # persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='test-volume', read_only=False),
-            host_path=k8s.V1HostPathVolumeSource(path=kwargs['context']['dag_run'].conf.get('ACCUTUNING_WORKSPACE')),
+            # name=kwargs['context']['dag_run'].conf.get('ACCUTUNING_PVC_NAME'),
+            # host_path=k8s.V1HostPathVolumeSource(path=kwargs['context']['dag_run'].conf.get('ACCUTUNING_WORKSPACE')),
+            name=env_vars_dict.get('ACCUTUNING_PVC_NAME'),
+            host_path=k8s.V1HostPathVolumeSource(path=env_vars_dict.get('ACCUTUNING_WORKSPACE')),
         )
         self.volumes = [volumes]
         self.arguments = kwargs['context']['task_instance'].xcom_pull(
@@ -85,7 +106,8 @@ class KubernetesPodExOperator(KubernetesPodOperator):
 
 command_worker = KubernetesPodExOperator(
     namespace='default',
-    image='{{dag_run.conf.ACCUTUNING_APP_IMAGE}}',
+    # image='{{dag_run.conf.ACCUTUNING_APP_IMAGE}}',
+    image=json.loads('{{dag_run.conf.accutuning_env_vars}}').get('ACCUTUNING_APP_IMAGE'),
     # image='pooh97/accu-app:latest',
     # volumes=[volume],
     # volume_mounts=[volume_mount],
