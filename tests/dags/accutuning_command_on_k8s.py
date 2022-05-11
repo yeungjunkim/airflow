f4670fc1c7a064e7032d0203001a705f36a9b616
yeungjunkim <pooh97@gmail.com>
2022-05-11 19:22:38 +0900

    Update accutuning_command_on_k8s.py

--- tests/dags/accutuning_command_on_k8s.py
+++ tests/dags/accutuning_command_on_k8s.py
@@ -28,7 +28,7 @@ start = DummyOperator(task_id='start', dag=dag)
 
 def make_env_var():
 
-    env_vars_dict = "{{context.dag_run.conf.accutuning_env_vars}}"
+    env_vars_dict = "{{dag_run.conf.accutuning_env_vars}}"
     # env_vars_dict = json.loads(kwargs['context']['dag_run'].conf.accutuning_env_vars)
     print("====================================")
     print(env_vars_dict)
