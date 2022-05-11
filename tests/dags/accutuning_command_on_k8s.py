d5f68df14eb0da67da958ca76ab8f8d4b71db25b
yeungjunkim <pooh97@gmail.com>
2022-05-11 22:20:58 +0900

    Update accutuning_command_on_k8s.py

--- tests/dags/accutuning_command_on_k8s.py
+++ tests/dags/accutuning_command_on_k8s.py
@@ -5,7 +5,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from kubernetes.client import models as k8s  # you should write this sentence when you could use volume, etc 
-import json
+import json, re
 
 default_args = {
     'owner': 'airflow',
@@ -29,18 +29,24 @@ start = DummyOperator(task_id='start', dag=dag)
 def make_env_var(**kwargs):
 
     dict_str = "{{ dag_run.conf.accutuning_env_vars }}"
-    dump_str = json.dumps(dict_str)
-    dict_str = json.loads(dump_str)
-    # json_str = json.loads(dict_str)
-    # clean_dict = json.dumps(dict_str)
+    change_str = re.sub(r'(?<=[^\\])\\(?=[^\\])', r'\\\\', repr(dict_str)[1:-1])
+    print(change_str)
+
+    # convert to dictionary
+    change_dict = json.loads(change_str)
+
+    # dump_str = json.dumps(dict_str)
+    # dict_str = json.loads(dump_str)
+    # # json_str = json.loads(dict_str)
+    # # clean_dict = json.dumps(dict_str)
     # change_dict = json.loads(dict_str)
     # env_dict_str = json.loads(kwargs['dag_run'].conf.accutuning_env_vars)
 
     env_dict = {
-        'ACCUTUNING_WORKSPACE': dict_str,
-        'ACCUTUNING_LOG_LEVEL': dict_str,
-        'ACCUTUNING_USE_LABELER': dict_str,
-        'ACCUTUNING_USE_CLUSTERING': dict_str,
+        'ACCUTUNING_WORKSPACE': change_dict.get("ACCUTUNING_WORKSPACE"),
+        'ACCUTUNING_LOG_LEVEL': change_dict.get("ACCUTUNING_LOG_LEVEL"),
+        'ACCUTUNING_USE_LABELER': change_dict.get("ACCUTUNING_USE_LABELER"),
+        'ACCUTUNING_USE_CLUSTERING': change_dict.get("ACCUTUNING_USE_CLUSTERING"),
         # 'DJANGO_SETTINGS_MODULE': dict_str,
         # 'ACCUTUNING_DB_ENGINE': (json.loads(dict_str)).get("ACCUTUNING_DB_ENGINE"),
         # 'ACCUTUNING_DB_HOST': (json.loads(dict_str)).get("ACCUTUNING_DB_HOST"),
