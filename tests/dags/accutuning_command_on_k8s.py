2e74c5c7e87eb1061e447fc634986aaea7856839
yeungjunkim <pooh97@gmail.com>
2022-05-12 15:19:40 +0900

    Update accutuning_command_on_k8s.py

--- tests/dags/accutuning_command_on_k8s.py
+++ tests/dags/accutuning_command_on_k8s.py
@@ -190,19 +190,19 @@ class KubernetesPodExOperator(KubernetesPodOperator):
         self.volumes = [volumes]
         self.arguments = kwargs['context']['task_instance'].xcom_pull(
             task_ids='make_parameters', key='command').split()
-        self.env_vars = {
-            'ACCUTUNING_WORKSPACE': env_dict_str.get("ACCUTUNING_WORKSPACE"),
-            'ACCUTUNING_LOG_LEVEL': env_dict_str.get("ACCUTUNING_LOG_LEVEL"),
-            'ACCUTUNING_USE_LABELER': env_dict_str.get("ACCUTUNING_USE_LABELER"),
-            'ACCUTUNING_USE_CLUSTERING': env_dict_str.get("ACCUTUNING_USE_CLUSTERING"),
-            'DJANGO_SETTINGS_MODULE': env_dict_str.get("DJANGO_SETTINGS_MODULE"),
-            'ACCUTUNING_DB_ENGINE': env_dict_str.get("ACCUTUNING_DB_ENGINE"),
-            'ACCUTUNING_DB_HOST': env_dict_str.get("ACCUTUNING_DB_HOST"),
-            'ACCUTUNING_DB_PORT': env_dict_str.get("ACCUTUNING_DB_PORT"),
-            'ACCUTUNING_DB_NAME': env_dict_str.get("ACCUTUNING_DB_NAME"),
-            'ACCUTUNING_DB_USER': env_dict_str.get("ACCUTUNING_DB_USER"),
-            'ACCUTUNING_DB_PASSWORD': env_dict_str.get("ACCUTUNING_DB_PASSWORD"),
-        }
+        # self.env_vars = {
+        #     'ACCUTUNING_WORKSPACE': env_dict_str.get("ACCUTUNING_WORKSPACE"),
+        #     'ACCUTUNING_LOG_LEVEL': env_dict_str.get("ACCUTUNING_LOG_LEVEL"),
+        #     'ACCUTUNING_USE_LABELER': env_dict_str.get("ACCUTUNING_USE_LABELER"),
+        #     'ACCUTUNING_USE_CLUSTERING': env_dict_str.get("ACCUTUNING_USE_CLUSTERING"),
+        #     'DJANGO_SETTINGS_MODULE': env_dict_str.get("DJANGO_SETTINGS_MODULE"),
+        #     'ACCUTUNING_DB_ENGINE': env_dict_str.get("ACCUTUNING_DB_ENGINE"),
+        #     'ACCUTUNING_DB_HOST': env_dict_str.get("ACCUTUNING_DB_HOST"),
+        #     'ACCUTUNING_DB_PORT': env_dict_str.get("ACCUTUNING_DB_PORT"),
+        #     'ACCUTUNING_DB_NAME': env_dict_str.get("ACCUTUNING_DB_NAME"),
+        #     'ACCUTUNING_DB_USER': env_dict_str.get("ACCUTUNING_DB_USER"),
+        #     'ACCUTUNING_DB_PASSWORD': env_dict_str.get("ACCUTUNING_DB_PASSWORD"),
+        # }
         # print("volume = {}".format(self.volume))
         self.image = env_dict_str.get("ACCUTUNING_APP_IMAGE"),
         return super().pre_execute(*args, **kwargs)
