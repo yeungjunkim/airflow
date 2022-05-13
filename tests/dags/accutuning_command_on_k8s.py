3375854834e46fbff289ee5980d7d69ab393ea69
yeungjunkim <pooh97@gmail.com>
2022-05-13 14:50:28 +0900

    Update accutuning_command_on_k8s.py

--- tests/dags/accutuning_command_on_k8s.py
+++ tests/dags/accutuning_command_on_k8s.py
@@ -76,21 +76,7 @@ class KubernetesPodExOperator(KubernetesPodOperator):
     )
 
     def __init__(self, *args, **kwargs):
-        env_dict_str = json.loads(kwargs['context']['dag_run'].conf.get("accutuning_env_vars"))
 
-        self.env_vars = {
-            "ACCUTUNING_WORKSPACE": env_dict_str.get("ACCUTUNING_WORKSPACE"),
-            "ACCUTUNING_LOG_LEVEL": env_dict_str.get("ACCUTUNING_LOG_LEVEL"),
-            "ACCUTUNING_USE_LABELER": env_dict_str.get("ACCUTUNING_USE_LABELER"),
-            "ACCUTUNING_USE_CLUSTERING": env_dict_str.get("ACCUTUNING_USE_CLUSTERING"),
-            "DJANGO_SETTINGS_MODULE": env_dict_str.get("DJANGO_SETTINGS_MODULE"),
-            "ACCUTUNING_DB_ENGINE": env_dict_str.get("ACCUTUNING_DB_ENGINE"),
-            "ACCUTUNING_DB_HOST": env_dict_str.get("ACCUTUNING_DB_HOST"),
-            "ACCUTUNING_DB_PORT": env_dict_str.get("ACCUTUNING_DB_PORT"),
-            "ACCUTUNING_DB_NAME": env_dict_str.get("ACCUTUNING_DB_NAME"),
-            "ACCUTUNING_DB_USER": env_dict_str.get("ACCUTUNING_DB_USER"),
-            "ACCUTUNING_DB_PASSWORD": env_dict_str.get("ACCUTUNING_DB_PASSWORD")
-        }
         super().__init__(*args, **kwargs)
 
     def pre_execute(self, *args, **kwargs):
@@ -121,7 +107,20 @@ class KubernetesPodExOperator(KubernetesPodOperator):
 
     def execute(self, *args, **kwargs):
         env_dict_str = json.loads(kwargs['context']['dag_run'].conf.get("accutuning_env_vars"))
-        self.env_vars = env_dict_str
+
+        self.env_vars = {
+            "ACCUTUNING_WORKSPACE": env_dict_str.get("ACCUTUNING_WORKSPACE"),
+            "ACCUTUNING_LOG_LEVEL": env_dict_str.get("ACCUTUNING_LOG_LEVEL"),
+            "ACCUTUNING_USE_LABELER": env_dict_str.get("ACCUTUNING_USE_LABELER"),
+            "ACCUTUNING_USE_CLUSTERING": env_dict_str.get("ACCUTUNING_USE_CLUSTERING"),
+            "DJANGO_SETTINGS_MODULE": env_dict_str.get("DJANGO_SETTINGS_MODULE"),
+            "ACCUTUNING_DB_ENGINE": env_dict_str.get("ACCUTUNING_DB_ENGINE"),
+            "ACCUTUNING_DB_HOST": env_dict_str.get("ACCUTUNING_DB_HOST"),
+            "ACCUTUNING_DB_PORT": env_dict_str.get("ACCUTUNING_DB_PORT"),
+            "ACCUTUNING_DB_NAME": env_dict_str.get("ACCUTUNING_DB_NAME"),
+            "ACCUTUNING_DB_USER": env_dict_str.get("ACCUTUNING_DB_USER"),
+            "ACCUTUNING_DB_PASSWORD": env_dict_str.get("ACCUTUNING_DB_PASSWORD")
+        }
 
         return super().execute(*args, **kwargs)
 
